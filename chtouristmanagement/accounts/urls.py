fd2114a96cc49f5d856191b3d174160f3587515c
adler <agapay.adril@gmail.com>
2023-01-11 16:38:11 +0800

    test commit, Logout Addition

--- chtouristmanagement/accounts/urls.py
+++ chtouristmanagement/accounts/urls.py
@@ -5,5 +5,6 @@ from knox import views as knox_views
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view())
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/logout', knox_views.LogoutAllView.as_view(), name='knox_logout')
 ]
